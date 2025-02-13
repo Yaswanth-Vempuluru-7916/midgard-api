0d6eed93f8c7cb9ff7026dfcb9c01b7d9127876f
yaswanth-vempuluru <yaswanthvempuluru@outlook.com>
2025-02-13 14:25:24 +0530

    changed the mistaken folder structure

---
+++ src/config/settings.rs
@@ -0,0 +1,22 @@
+use dotenvy::dotenv;
+use std::env;
+
+pub struct Settings{
+    pub mongo_uri  : String,
+    pub port : u16,
+}
+
+impl Settings{
+    pub fn new() -> Self {
+
+        dotenv().ok(); // Load .env file
+
+        Self {
+            mongo_uri: env::var("MONGO_URI").expect("MONGO_URI to be set"),
+            port : env::var("PORT")
+            .unwrap_or_else(|_| "8080".to_string())
+            .parse() //Converts the string "8080" to a u16 (integer).
+            .expect("PORT must be a valid number")            
+        }
+    }
+}
